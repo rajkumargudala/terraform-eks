286906382f252013029567d9c75219579e000674
Rajkumar Gudala <gudalarajkumar4444@gmail.com>
2026-08-08 21:58:48 +0530

    Modify backend S3 bucket and region in main.tf
    Updated S3 bucket name and region for backend configuration.

--- eks-install/main.tf
+++ eks-install/main.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "demo-terraform-eks-state-s3-bucket"
+    bucket         = "demo-terraform-eks-state-s3-bucket1234444446"
     key            = "terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-1"
     dynamodb_table = "terraform-eks-state-locks"
     encrypt        = true
   }
